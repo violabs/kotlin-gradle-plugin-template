d4c41d5c2b8c4c563788610310a138c1afef2056
Violabs Josh <139578299+violabs-josh@users.noreply.github.com>
2024-06-27 21:06:38 -0500

    Initial commit

---
+++ src/main/kotlin/io/violabs/template/TemplateApplication.kt
@@ -0,0 +1,11 @@
+package io.violabs.template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<TemplateApplication>(*args)
+}
---
+++ src/test/kotlin/io/violabs/template/TemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.violabs.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
